5dd7216eba058fa4a721384afe1762b9827b26ce
juanmisaez <juanmiguelsaezh@gmail.com>
2022-02-15 12:29:52 +0100

    Versión 1.4.1
    -Eliminar packages de assets
    -Uso de assets de seguidores
    -Cambio de movimiento de controller a rigidbody
    -Crear prefab del player
    -Mover prefabs a su carpeta
    -Añadido seguimiento de soldados por texto al canvas (solo visual)

--- Assets/Scripts/Controllers/CompanionController.cs
+++ Assets/Scripts/Controllers/CompanionController.cs
@@ -4,32 +4,24 @@ using UnityEngine;
 
 public class CompanionController : MonoBehaviour
 {
+    private Rigidbody _rb;
     private InputSystemKeyboard _input;
-    private CharacterController _controller;
-
 
     public float speed;
-    public float gravity = -9.81f; // falta terminar
-
-    Vector3 velocity;
 
     private void Awake()
     {
+        _rb = GetComponent<Rigidbody>();
         _input = GetComponent<InputSystemKeyboard>();
-        _controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        _controller.Move(transform.forward * speed * Time.deltaTime);
+        _rb.transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         if (_input.hor != 0)
         {
-            _controller.Move(Vector3.right * speed * _input.hor * Time.deltaTime);
+            _rb.transform.Translate(Vector3.right * speed * _input.hor * Time.deltaTime);
         }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        _controller.Move(velocity * Time.deltaTime);
     }
 }
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,36 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //private Rigidbody _rb;
+    private Rigidbody _rb;
     private Animator _anim;
     private InputSystemKeyboard _input;
-    private CharacterController _controller;
 
     public float speed;
-    public float gravity = -9.81f; // falta terminar
-
-    Vector3 velocity;
 
     private void Awake()
     {
-        //_rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
         _anim = GetComponent<Animator>();
         _input = GetComponent<InputSystemKeyboard>();
-        _controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        _controller.Move(transform.forward * speed * Time.deltaTime);
+        _rb.transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         if (_input.hor != 0)
         {
-            _controller.Move(Vector3.right * speed * _input.hor * Time.deltaTime);
+            _rb.transform.Translate(Vector3.right * speed * _input.hor * Time.deltaTime);
         }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        _controller.Move(velocity * Time.deltaTime);
     }
 
     void Tripping()
